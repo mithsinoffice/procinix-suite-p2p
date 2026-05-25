545cf523f71e1b540e0f4623faba7f30800d3d4d
Mithilesh Tiwari <mithsinoffice@gmail.com>
2026-05-25 11:09:38 +0530

    fix(deploy): disable CSP to unblock Vite inline script

--- server/src/plugins/helmet.ts
+++ server/src/plugins/helmet.ts
@@ -1,21 +1,9 @@
 import fp from 'fastify-plugin'
 import helmetPlugin from '@fastify/helmet'
 import type { FastifyInstance } from 'fastify'
-
 export const helmetConfig = fp(async (app: FastifyInstance) => {
   await app.register(helmetPlugin, {
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc:  ["'self'"],
-        styleSrc:   ["'self'", "'unsafe-inline'"],
-        imgSrc:     ["'self'", 'data:', 'https:'],
-        connectSrc: ["'self'"],
-        fontSrc:    ["'self'"],
-        objectSrc:  ["'none'"],
-        frameSrc:   ["'none'"],
-      },
-    },
+    contentSecurityPolicy: false,
     hsts: { maxAge: 31536000, includeSubDomains: true, preload: true },
     noSniff: true,
     xssFilter: true,
